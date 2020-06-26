abdfb8ff171aa9aeb4ce6c3fbdef605a44ebebd4
akayayse <47823013+akayayse@users.noreply.github.com>
2020-06-27 02:09:01 +0300

    Add files via upload

---
+++ Number spiral diagonals.js
@@ -0,0 +1,19 @@
+const range =n => {
+    res = []
+    for ( let i = 1; i<=n; i+=2)
+        res.push(i*i)
+    return res
+}
+
+let giveAddition =arr => {
+    let res =[]
+    arr.forEach((n,i) =>{
+        let start = arr[i-1] || 0
+        let add = (n - arr[i-1])/4 || 1
+        for (let j = start+add; j<=n; j+=add)
+            res.push(j)
+    })
+    return res
+}
+
+console.log(giveAddition(range(1001)).reduceRight((x,y)=>{return x+y}))
